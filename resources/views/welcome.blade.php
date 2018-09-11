dbc1d72f4b7e7758fa46712834bb7e2dbf4feb2e
Mac <mac@Macs-MacBook-Pro-3.local>
2018-09-11 18:31:39 +0700

    add style home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,13 +72,14 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        {{-- <a href="{{ route('login') }}">Login</a> --}}
                         <a href="{{ route('create') }}">Register</a>
                     @endauth
                 </div>
             @endif
 
-            <div class="content">fdadfasdfasd∂ßßßßß
+            <div class="content background">
+                {{-- <img src="img/background.jpg" alt=""> --}}
             </div>
         </div>
     </body>
