394a5e3ae3420a9cbea69d1da1c3e59612ad24ac
Mac <mac@Macs-MacBook-Pro-3.local>
2018-09-13 15:35:56 +0700

    fix style

--- resources/views/frontend/student-register/index.blade.php
+++ resources/views/frontend/student-register/index.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="panel panel-default">
-                <div class="panel-heading"><h1 class="font_title">អរគុណដែលបានចុះឈ្មោះរួច</h1></div>
+                <div class="panel-heading"><h1 class="font_title">អរគុណដែលបានចុះឈ្មោះរួច។</h1></div>
                 <div class="panel-body">
                     <p>សូមប្អនៗចូលទៅកាន់ Link <a href="{{ url('https://www.facebook.com/ceoinstitute2014/') }}">CEO INSTITUTE</a> </p>
                 </div>
