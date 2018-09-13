8c184c086a57ce4911b2b5e2280a7e92dfbef5c6
Mac <mac@Macs-MacBook-Pro-3.local>
2018-09-13 18:48:22 +0700

    add style

--- resources/views/frontend/homepage/index.blade.php
+++ resources/views/frontend/homepage/index.blade.php
@@ -3,84 +3,79 @@
 
 @section('content')
 
+
 <div class="container-fluid well">
 	<div class="row">
 		<div class="container">
 			<div class="col-md-12">
 				<section class="features3 cid-qv5zpCY6d9" id="features3-1j" data-rv-view="4727">
+				    <div class="container">
+				        <div class="media-container-row">
+				            <div class="card p-3 col-12 col-md-6 col-lg-4">
+				                <div class="card-wrapper">
+				                    <div class="card-img">
+				                        <img class="img-responsive thumbnail" src="{{ asset('img/01.jpg') }}" alt="Mobirise" media-simple="true">
+				                    </div>
+				                    <div class="card-box">
+				                        <h4 class="card-title mbr-fonts-style display-7">
+				                            No Coding
+				                        </h4>
+				                        <p class="mbr-text mbr-fonts-style display-7">
+				                            Mobirise is an easy website builder - just drop site elements to your page, add content and style it to look the way you like.
+				                        </p>
+				                    </div>
+				                    <div class="mbr-section-btn text-center">
+				                        <a href="https://mobirise.com" class="btn btn-primary display-4">
+				                            Learn More
+				                        </a>
+				                    </div>
+				                </div>
+				            </div>
 
-    
+				            <div class="card p-3 col-12 col-md-6 col-lg-4">
+				                <div class="card-wrapper">
+				                    <div class="card-img">
+				                        <img class="img-responsive thumbnail" src="{{ asset('img/02.jpg') }}" alt="Mobirise" media-simple="true">
+				                    </div>
+				                    <div class="card-box">
+				                        <h4 class="card-title mbr-fonts-style display-7">
+				                            Mobile Friendly
+				                        </h4>
+				                        <p class="mbr-text mbr-fonts-style display-7">
+				                            All sites you make with Mobirise are mobile-friendly. You don't have to create a special mobile version of your site.
+				                        </p>
+				                    </div>
+				                    <div class="mbr-section-btn text-center">
+				                        <a href="https://mobirise.com" class="btn btn-primary display-4 display-block">
+				                            Learn More
+				                        </a>
+				                    </div>
+				                </div>
+				            </div>
 
-    
-    <div class="container">
-        <div class="media-container-row">
-            <div class="card p-3 col-12 col-md-6 col-lg-4">
-                <div class="card-wrapper">
-                    <div class="card-img">
-                        <img class="img-responsive thumbnail" src="{{ asset('img/01.jpg') }}" alt="Mobirise" media-simple="true">
-                    </div>
-                    <div class="card-box">
-                        <h4 class="card-title mbr-fonts-style display-7">
-                            No Coding
-                        </h4>
-                        <p class="mbr-text mbr-fonts-style display-7">
-                            Mobirise is an easy website builder - just drop site elements to your page, add content and style it to look the way you like.
-                        </p>
-                    </div>
-                    <div class="mbr-section-btn text-center">
-                        <a href="https://mobirise.com" class="btn btn-primary display-4">
-                            Learn More
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card p-3 col-12 col-md-6 col-lg-4">
-                <div class="card-wrapper">
-                    <div class="card-img">
-                        <img class="img-responsive thumbnail" src="{{ asset('img/02.jpg') }}" alt="Mobirise" media-simple="true">
-                    </div>
-                    <div class="card-box">
-                        <h4 class="card-title mbr-fonts-style display-7">
-                            Mobile Friendly
-                        </h4>
-                        <p class="mbr-text mbr-fonts-style display-7">
-                            All sites you make with Mobirise are mobile-friendly. You don't have to create a special mobile version of your site.
-                        </p>
-                    </div>
-                    <div class="mbr-section-btn text-center">
-                        <a href="https://mobirise.com" class="btn btn-primary display-4">
-                            Learn More
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card p-3 col-12 col-md-6 col-lg-4">
-                <div class="card-wrapper">
-                    <div class="card-img">
-                        <img class="img-responsive thumbnail" src="{{ asset('img/03.jpg') }}" alt="Mobirise" media-simple="true">
-                    </div>
-                    <div class="card-box">
-                        <h4 class="card-title mbr-fonts-style display-7">
-                            Unique Styles
-                        </h4>
-                        <p class="mbr-text mbr-fonts-style display-7">
-                            Mobirise offers many site blocks in several themes, and though these blocks are pre-made, they are flexible.
-                        </p>
-                    </div>
-                    <div class="mbr-section-btn text-center">
-                        <a href="https://mobirise.com" class="btn btn-primary display-4">
-                            Learn More
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            
-        </div>
-    </div>
-</section>
+				            <div class="card p-3 col-12 col-md-6 col-lg-4">
+				                <div class="card-wrapper">
+				                    <div class="card-img">
+				                        <img class="img-responsive thumbnail" src="{{ asset('img/03.jpg') }}" alt="Mobirise" media-simple="true">
+				                    </div>
+				                    <div class="card-box">
+				                        <h4 class="card-title mbr-fonts-style display-7">
+				                            Unique Styles
+				                        </h4>
+				                        <p class="mbr-text mbr-fonts-style display-7">
+				                            Mobirise offers many site blocks in several themes, and though these blocks are pre-made, they are flexible.
+				                        </p>
+				                    </div>
+				                    <div class="mbr-section-btn text-center">
+				                        <a href="https://mobirise.com" class="btn btn-primary display-4">
+				                            Learn More
+				                        </a>
+				                    </div>
+				                </div>
+				            </div>
+				        </div>
+				    </div>
+				</section>
 			</div>
 		</div>
 	</div>
